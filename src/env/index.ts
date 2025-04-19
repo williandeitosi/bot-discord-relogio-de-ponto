cef035fefb20ca4ed8a6ae9de201e52f3d1bc96c
williandeitosi <williandeitosi@gmail.com>
2025-04-19 11:10:40 -0300

    initial settings

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import z from "zod";
+
+const envSchema = z.object({
+  APPLICATION_ID: z.coerce.number(),
+  PUBLIC_KEY: z.string(),
+  BOT_TOKEN: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("âŒ Invalid environments variables", _env.error.format());
+
+  throw new Error("Invalid environments variables");
+}
+
+export const env = _env.data;
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const text = "Ola Bot";
+console.log(text);
